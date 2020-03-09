822cbe35e02e01cbccfa8fe179c3708ec6a98763
Hermano Leite <hermano@hermano-pc>
2020-03-09 07:58:49 -0300

    melhoria no botao palpite

--- src/app/components/jogo/botao-palpite/botao-palpite.component.ts
+++ src/app/components/jogo/botao-palpite/botao-palpite.component.ts
@@ -1,14 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Etapa } from 'src/app/containers/jogo/jogo.status';
+import { AngularFirestoreDocument } from '@angular/fire/firestore';
 
 @Component({
   selector: 'botao-palpite',
   templateUrl: './botao-palpite.component.html',
 })
-export class BotaoPalpiteComponent implements OnInit {
+export class BotaoPalpiteComponent {
+  @Input() etapa : Etapa;
+  @Input() rodadaDoc: AngularFirestoreDocument;
+  @Input() jogador: any;
+  @Input() rodada: any;
+  constructor() { 
+    console.log("-------------- construindo botao palpite");
+  }
+  
+  etapaPalpite() {
+    return this.etapa === Etapa.palpite;
+  }
+  
+  palpite(palpite: number) : void {
+    const jogadorDoc = this.rodadaDoc.collection("jogadores").doc(this.jogador.id.toString());
+    jogadorDoc.update({ palpite: palpite });
+    
+    const proximoJogador = this.proximoJogador();
 
-  constructor() { }
-
-  ngOnInit() {
+    if (this.rodada.comeca === this.rodada.vez) {
+      this.criarJogada(proximoJogador)
+      this.rodadaDoc.update({ etapa: Etapa.jogarCarta, vez: proximoJogador });
+    }
+    else {
+      this.rodadaDoc.update({ vez: proximoJogador });
+    }
   }
 
+  proximoJogador() : number {
+    const vez = this.rodada.vez + 1;
+    return vez === this.rodada.jogadoresCount ? 0 : vez;
+  }
+
+  criarJogada(jogadorComeca) : void {
+    const jogadaCollection = this.rodadaDoc.collection("jogada");
+
+    const jogada = {
+      comeca: jogadorComeca,
+      maiorCarta: null,
+    }
+
+    jogadaCollection.add(jogada).then(function(docRef) {
+      this.rodadaDoc.update({ jogadaAtual: docRef.id, vez: jogadorComeca });
+    }.bind(this))
+    .catch(function(error) {
+      console.error("Error adding document: ", error);
+    });
+  }
 }
