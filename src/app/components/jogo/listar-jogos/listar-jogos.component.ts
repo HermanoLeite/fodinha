255f890f8d8d7c05eb6211b0468d2c5e9c939bd2
Hermano Leite <hplleite@gmail.com>
2020-04-10 20:24:42 -0300

    enviando jogos por paramentro

--- src/app/components/jogo/listar-jogos/listar-jogos.component.ts
+++ src/app/components/jogo/listar-jogos/listar-jogos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Status } from 'src/app/containers/jogo/jogo.status';
 import { Router } from '@angular/router';
 import { JogoService } from 'src/app/service/jogo.service';
@@ -8,8 +8,8 @@ import { Observable } from 'rxjs';
   selector: 'listar-jogos',
   templateUrl: './listar-jogos.component.html'
 })
-export class ListarJogosComponent implements OnInit {
-  private jogos: Observable<any>;
+export class ListarJogosComponent {
+  @Input() jogos: Observable<any>;
   constructor(
     private router: Router,
     private jogoService: JogoService) {
@@ -34,9 +34,5 @@ export class ListarJogosComponent implements OnInit {
 
   deletarJogo(jogoId) {
     this.jogoService.deletarJogo(jogoId)
-  }
-
-  ngOnInit() {
-    this.jogos = this.jogoService.jogosStream()
   }
 }
--- src/app/containers/jogo-init/jogo-init.component.ts
+++ src/app/containers/jogo-init/jogo-init.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { LocalStorageService } from 'src/app/service/local-storage';
+import { Router } from '@angular/router';
+import { JogoService } from 'src/app/service/jogo.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-jogo-init',
   templateUrl: './jogo-init.component.html',
 })
 export class JogoInitComponent {
-  constructor(private localStorageService: LocalStorageService) {
+  private jogos: Observable<any>;
+
+  constructor(
+    private localStorageService: LocalStorageService,
+    private router: Router,
+    private jogoService: JogoService) {
     this.localStorageService.clear()
+  }
+
+  ngOnInit() {
+    this.jogos = this.jogoService.jogosStream()
   }
 }
