fb05478fcd03d2b82b9ffe967caa01e5bda5883f
Hermano Leite <hplleite@gmail.com>
2020-05-03 08:13:12 -0300

    carta aparecendo de forma feiosa no placar

--- src/app/components/jogo/components/placar-rodada/placar-rodada.component.ts
+++ src/app/components/jogo/components/placar-rodada/placar-rodada.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Carta } from 'src/app/models/carta.model';
 
 @Component({
   selector: 'placar-rodada',
@@ -6,7 +7,18 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./placar-rodada.component.css'],
 })
 export class PlacarRodadaComponent {
-  @Input() jogadores;
-  @Input() rodada;
-  constructor() { }
+  @Input() jogadores
+  @Input() rodada
+  @Input() jogadas
+  @Input() visaoCarta
+  public cartas = []
+
+  ngOnChanges() {
+    if (this.jogadas && this.jogadas.length > 0) {
+      this.jogadas.map(jogada => this.cartas[jogada.jogadorId] = Carta.fromJogada(jogada))
+    }
+    else {
+      this.cartas = []
+    }
+  }
 }
